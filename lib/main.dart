d1815dfea1b5feb740bf9f5ba348ce1c3aa650a0
gaoguanqi <307590625@qq.com>
2019-04-30 14:41:40 +0800

    重启APP方案

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,49 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+    return RestartWidget(
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+        ),
+        home: BottomNavigationWidget(),
       ),
-      home: BottomNavigationWidget(),
     );
   }
 }
+
+//这个组件用来重新加载整个child Widget的。当我们需要重启APP的时候，可以使用这个方案
+//https://stackoverflow.com/questions/50115311/flutter-how-to-force-an-application-restart-in-production-mode
+class RestartWidget extends StatefulWidget {
+  final Widget child;
+
+  RestartWidget({this.child});
+
+  static restartApp(BuildContext context) {
+    final _RestartWidgetState state =
+    context.ancestorStateOfType(const TypeMatcher<_RestartWidgetState>());
+    state.restartApp();
+  }
+
+  @override
+  _RestartWidgetState createState() => new _RestartWidgetState();
+}
+
+class _RestartWidgetState extends State<RestartWidget> {
+  Key key = new UniqueKey();
+
+  void restartApp() {
+    this.setState(() {
+      key = new UniqueKey();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      key: key,
+      child: widget.child,
+    );
+  }
+}
+
